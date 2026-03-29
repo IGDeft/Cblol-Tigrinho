ccfcadc5e17211dc0c6394423602409b510ef9fc
Janaína Ferreira Monteiro <janainaferreiramonteiro@hotmail.com>
2026-03-29 18:18:59 -0300

    feat: criação da rota api

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+# import uvicorn
+# from seu_arquivo_da_ia import seu_metodo_que_retorna_array # Importa sua função
+
+# app = FastAPI()
+
+# @app.get("/predict")
+# def predict():
+#     dados_ia = seu_metodo_que_retorna_array()
+#     return {"resultado": dados_ia}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=5000)
