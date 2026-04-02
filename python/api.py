1daf2cc2cbafb65688d7786decff8b0937522e10
Janaína Ferreira Monteiro <janainaferreiramonteiro@hotmail.com>
2026-04-02 18:41:03 -0300

    feat: api python agr recebe time A e B, e qntd de jogos

--- python/api.py
+++ python/api.py
@@ -3,10 +3,19 @@ import uvicorn
 from cblol import ordemPicksBans
 app = FastAPI()
 
+@app.post("/predict")
+def predfict(data: dict = Body(...)):
+    time_a = data.get("timeA")
+    time_b = data.get("timeB")
+    jogos = data.get("quantidadeJogos")
+
+    response = ordemPicksBans(time_a, time_b, jogos, [])
+
+    return {"draft": response}
+
 @app.get("/")
 def home():
-    response = ordemPicksBans("LOUD", "paiN Gaming", 2, [])
-    return {"status": response}
+    return {"status": "Python API is running"}
 
 if __name__ == "__main__":
     uvicorn.run("api:app", host="0.0.0.0", port=5000, reload=True)
