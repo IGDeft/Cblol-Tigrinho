16b984a30be02e15bc736eb75114df03613c9a3c
Janaína Ferreira Monteiro <janainaferreiramonteiro@hotmail.com>
2026-04-03 16:12:48 -0300

    feat: adicionei as rotas novas no api.py

--- python/api.py
+++ python/api.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI, Body
 import uvicorn 
-from cblol import ordemPicksBans
+import cblol
 app = FastAPI()
 
+# GET
+@app.get("/")
+def home():
+    return {"status": "Python API is running"}
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="0.0.0.0", port=5000, reload=True)
+
+
+@app.get("/ligas")
+def listar_ligas():
+    response = cblol.listar_todas_ligas()
+    return {"ligas": response}
+
+
+@app.get("/times")
+def listar_times(data: dict = Body(...)):
+    ligas = data.get("ligas", "LTA S")
+    response = cblol.obter_times_liga(ligas)
+    return {"times": response}
+
+# POST
 @app.post("/predict")
 def predict(data: dict = Body(...)):
     print(f"Dados recebidos do Java: {data}")
@@ -10,14 +32,7 @@ def predict(data: dict = Body(...)):
     time_b = data.get("timeB")
     jogos = data.get("quantidadeJogos")
 
-    response = ordemPicksBans(time_a, time_b, jogos, [])
+    response = cblol.ordemPicksBans(time_a, time_b, jogos, [])
 
     return {"draft": response}
-
-@app.get("/")
-def home():
-    return {"status": "Python API is running"}
-
-if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=5000, reload=True)
 
